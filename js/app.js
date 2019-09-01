c1e7d34358d7d58fada607f2730fe16059b6ce23
Rafael Sant' Anna <rafaeel.16@gmail.com>
2019-08-31 23:11:02 -0300

    initial setup

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBJb6hOvbXGWxLvqbAiyFZErREBNfPE4ro",
+    authDomain: "giftlist-e27ea.firebaseapp.com",
+    databaseURL: "https://giftlist-e27ea.firebaseio.com",
+    projectId: "giftlist-e27ea",
+    storageBucket: "",
+    messagingSenderId: "813499865296",
+    appId: "1:813499865296:web:65e603c3585e4239"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
